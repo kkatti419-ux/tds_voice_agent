3a47a0c0a6260a14676af0d126746c1e00267b2a
kkatti419-ux <kkatti419@gmail.com>
2026-04-07 10:11:15 +0530

    first commit

---
+++ lib/design_system/widgets/primary_button.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:tds_voice_agent/core/utils/widgets/design_system/colors.dart';
+
+class AppButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const AppButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(backgroundColor: AppColors.primary),
+      onPressed: onPressed,
+      child: Text(text),
+    );
+  }
+}
